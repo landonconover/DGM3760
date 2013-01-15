cea67dc363a9b912fc0997fed7cb4ffb514a4eec
Landon Conover <landon.conover@uvu.edu>
2013-01-15 16:29:55 -0700

    Base Template and Js done

--- assign2/js/main.js
+++ assign2/js/main.js
@@ -1,7 +1,18 @@
 (function() {
 
 var myForm = $('#contactForm');
+var empDialog = $('#addNewEmpDialog');
 
+//init the dialog
+empDialog.dialog({ autoOpen: false, modal: true });
+
+//open the dialog when the button is pressed
+$('#addNew').click(function(){
+    empDialog.dialog( "open" )
+})
+
+
+//do this when the form is submitted
 myForm.submit(function(e){
     $.post("sendMail.php", myForm.serialize(), function(data){
         console.log(data);
