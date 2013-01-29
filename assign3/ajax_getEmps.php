55b4743a2107edb138c5fbf1d782f5b91de3e86c
Landon Conover <landon.conover@uvu.edu>
2013-01-28 18:20:58 -0700

    Assign 3 init commit

---
+++ assign3/ajax_getEmps.php
@@ -0,0 +1,14 @@
+<?php
+require 'connect2db.php';
+
+$query = "SELECT * FROM empTable";
+$result = mysqli_query($link, $query);
+
+
+$rows = array();
+
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $rows[] = $row;
+}
+
+print json_encode($rows);
---
+++ assign3/js/main.js
@@ -0,0 +1,48 @@
+(function () {
+
+    var myForm = $('#contactForm');
+    var empDialog = $('#addNewEmpDialog');
+
+    //init the dialog
+    empDialog.dialog({
+        autoOpen: false,
+        modal: true,
+        show: "slide",
+        hide: "explode"
+    });
+
+    // Complile Handlebars templates - I could precompile these...
+    var empTypes = Handlebars.compile($("#reasonTemplate").html());
+    var empTemplate = Handlebars.compile($("#empRowTemplate").html());
+
+    //open the dialog when the button is pressed
+    $('#addNew').click(function () {
+        empDialog.dialog("open");
+    });
+
+    //get the empTypes and put them in the template.
+    $.getJSON('ajax_getEmpTypes.php', function (json, textStatus) {
+        $('#types').append(empTypes(json));
+    });
+
+    //get the emps and put them in the template
+    $.getJSON('ajax_getEmps.php', function (json, textStatus) {
+        $('#empBody').append(empTemplate(json));
+    });
+
+    //do this when the form is submitted
+    myForm.submit(function (e) {
+        $.post("ajax_addEmp.php", myForm.serialize(), function (data) {
+            console.log(data);
+            //append the new data
+            $('#empBody').append(empTemplate(data));
+            //reset the form
+            myForm[0].reset();
+            empDialog.dialog("close");
+
+        }, "json");
+
+        return false;
+    });
+
+}());
